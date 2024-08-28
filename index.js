f518e6d03930afc344fd8a1fbb0aba439eff33fc
Varnik <varnik.gupta@comprotechnologies.com>
2024-08-28 16:00:46 +0530

    powers

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var tileList,
   noOfStrTiles,
   strTileList,
   maxScore,
+  activePowers,
+  noOfPowers,
   playerName = "",
   start = 0;
 
@@ -31,8 +33,8 @@ var ball = {
   y: 0,
   color: "red",
   radius: 4,
-  spdX: 4,
-  spdY: 4,
+  spdX: 3,
+  spdY: 3,
   moving: false,
 };
 
@@ -46,6 +48,27 @@ var strongTile = {
   color: "blue",
   width: 25,
   height: 10,
+};
+
+var powerobj = {
+  height: 4,
+  width: 2,
+};
+
+var powers = {
+  width: {
+    color: "blue",
+    callback: function () {
+      base.width += 10;
+      drawBase();
+    },
+  },
+  decreaseLive: {
+    color: "red",
+    callback: function () {
+      lives -= 1;
+    },
+  },
 };
 
 document.addEventListener("keydown", function (Event) {
@@ -79,7 +102,7 @@ document.addEventListener("keyup", function (event) {
 document.getElementById("name").addEventListener("input", function () {
   let inputValue = this.value;
   playerName = this.value;
-  console.log("Current input value: ", inputValue);
+  // console.log("Current input value: ", inputValue);
 });
 
 drawBase = function () {
@@ -108,10 +131,16 @@ drawStrTile = function (obj) {
 drawTile = function (obj) {
   ctx.save();
   ctx.fillStyle = tile.color;
-  if (obj.corner == true)
-    ctx.fillRect(obj.x, obj.y, 10.5, tile.height);
-  else
-    ctx.fillRect(obj.x, obj.y, tile.width, tile.height);
+  if (obj.corner == true) ctx.fillRect(obj.x, obj.y, 10.5, tile.height);
+  else ctx.fillRect(obj.x, obj.y, tile.width, tile.height);
+  ctx.restore();
+};
+
+drawPowerUp = function (obj) {
+  // console.log(obj);
+  ctx.save();
+  ctx.fillStyle = powers[obj.type].color;
+  ctx.fillRect(obj.x, obj.y, powerobj.width, powerobj.height);
   ctx.restore();
 };
 
@@ -142,8 +171,8 @@ function renderLeaderboard() {
 
 collisionBaseBall = function (base, ball) {
   if (
-    ball.y + ball.radius >= base.y &&
-    ball.y - ball.radius <= base.y + base.height &&
+    ball.y + ball.radius == base.y &&
+    // ball.y - ball.radius <= base.y + base.height &&
     ball.x + ball.radius >= base.x &&
     ball.x - ball.radius <= base.x + base.width
   ) {
@@ -201,6 +230,8 @@ checkLives = function () {
     ball.x = base.x + 36;
     ball.y = base.y - 4;
     ball.moving = false;
+    base.width = 75;
+    activePowers = [];
     return true;
   }
   return false;
@@ -213,6 +244,7 @@ gameWin = function () {
     ctx.font = "30px  comic sans MS ";
     ctx.fillText("You Win ..!", 70, 80);
     ctx.restore();
+    activePowers = [];
     clearInterval(intervalVal);
     setHighSore(Math.max(score, maxScore));
     updateLeaderboard(playerName, score);
@@ -227,7 +259,7 @@ updateData = function () {
 
   let livesEle = document.getElementById("lives");
   livesEle.innerHTML = lives;
-}
+};
 
 gameOverMsg = function () {
   ctx.save();
@@ -235,6 +267,7 @@ gameOverMsg = function () {
   ctx.font = "30px  comic sans MS ";
   ctx.fillText("Game Over ..!", 70, 80);
   ctx.restore();
+  activePowers = [];
   setHighSore(Math.max(score, maxScore));
   updateLeaderboard(playerName, score);
   renderLeaderboard();
@@ -249,20 +282,20 @@ restartBtn = function () {
     ball.moving = false;
     startGame();
   };
-}
+};
 
 setHighSore = function (value) {
   console.log("set", value);
   localStorage.setItem("score", value);
   document.getElementById("highScore").innerHTML = value;
-}
+};
 
 fetchHighScore = function () {
   let score = localStorage.getItem("score");
   score = score ? parseInt(score) : 0;
   console.log("fetched", score);
   return score;
-}
+};
 
 updateGame = function () {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -273,6 +306,22 @@ updateGame = function () {
   tileList.forEach((x) => drawTile(x));
   drawBase();
   drawBall();
+  activePowers.forEach((power, index) => {
+    power.y += 2;
+    if (
+      power.y + powerobj.height >= base.y &&
+      power.x + powerobj.width >= base.x &&
+      power.x <= base.x + base.width
+    ) {
+      powers[power.type].callback();
+      activePowers.splice(index, 1);
+    } else if (power.y > ctx.canvas.height) {
+      activePowers.splice(index, 1);
+    } else {
+      drawPowerUp(power);
+    }
+  });
+
   if (ball.moving) {
     collisionBaseBall(base, ball);
     updateBasePosition();
@@ -282,6 +331,20 @@ updateGame = function () {
 
     for (key in strTileList) {
       if (collisionBallTile(strTileList[key])) {
+        if (strTileList[key].power == 1) {
+          activePowers[noOfPowers++] = {
+            type: "width",
+            x: strTileList[key].x,
+            y: strTileList[key].y,
+          };
+        }
+        if (strTileList[key].power == 2) {
+          activePowers[noOfPowers++] = {
+            type: "decreaseLive",
+            x: strTileList[key].x,
+            y: strTileList[key].y,
+          };
+        }
         score++;
         strTileList[key].strength--;
         if (strTileList[key].strength == 0) delete strTileList[key];
@@ -291,6 +354,20 @@ updateGame = function () {
 
     for (key in tileList) {
       if (collisionBallTile(tileList[key])) {
+        if (tileList[key].power == 1) {
+          activePowers[noOfPowers++] = {
+            type: "width",
+            x: tileList[key].x,
+            y: tileList[key].y,
+          };
+        }
+        if (tileList[key].power == 2) {
+          activePowers[noOfPowers++] = {
+            type: "decreaseLive",
+            x: tileList[key].x,
+            y: tileList[key].y,
+          };
+        }
         delete tileList[key];
         score++;
         ball.spdY *= -1;
@@ -309,6 +386,7 @@ updateGame = function () {
 startGame = function () {
   base.x = 115;
   base.y = 135;
+  base.width = 75;
   ball.x = base.x + 36;
   ball.y = base.y - 4;
 
@@ -316,6 +394,7 @@ startGame = function () {
   lives = 3;
   noOfTiles = 0;
   noOfStrTiles = 0;
+  noOfPowers = 0;
 
   maxScore = fetchHighScore();
 
@@ -324,11 +403,17 @@ startGame = function () {
 
   tileList = [];
   strTileList = [];
+  activePowers = [];
 
   for (var i = 1; i <= 3; ++i) {
     var strTileX = 7;
     for (var j = 1; j < 11; ++j) {
-      strTileList[noOfStrTiles++] = { x: strTileX, y: strTileY, strength: 2 };
+      strTileList[noOfStrTiles++] = {
+        x: strTileX,
+        y: strTileY,
+        strength: 2,
+        power: null,
+      };
       strTileX += 29;
     }
     strTileY += 28;
@@ -336,14 +421,25 @@ startGame = function () {
 
   for (var i = 1; i < 3; ++i) {
     var tileX = 7;
-    tileList[noOfTiles++] = { x: tileX, y: tileY, corner: true };
+    tileList[noOfTiles++] = { x: tileX, y: tileY, corner: true, power: null };
     tileX += 14;
     for (var j = 1; j < 10; ++j) {
-      tileList[noOfTiles++] = { x: tileX, y: tileY };
+      tileList[noOfTiles++] = { x: tileX, y: tileY, power: null };
       tileX += 29;
     }
-    tileList[noOfTiles++] = { x: tileX, y: tileY, corner: true };
+    tileList[noOfTiles++] = { x: tileX, y: tileY, corner: true, power: null };
     tileY += 28;
+  }
+
+  var combinedList = [...strTileList, ...tileList];
+
+  for (let i = combinedList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [combinedList[i], combinedList[j]] = [combinedList[j], combinedList[i]];
+  }
+
+  for (let i = 0; i < 5; i++) {
+    combinedList[i].power = Math.floor(Math.random() * 2 + 1);
   }
 
   document.getElementById("data").style.visibility = "hidden";
@@ -364,6 +460,9 @@ startMsg = function () {
   ctx.restore();
 
   document.getElementById("arrow").onclick = function () {
+    if (playerName == "") {
+      alert("Enter the player name to start!..");
+    }
     if (start == 0 && playerName != "") {
       start = 1;
       startGame();
@@ -376,4 +475,3 @@ if (start == 0) {
   document.getElementById("highScore").innerHTML = fetchHighScore();
   startMsg();
 }
-
